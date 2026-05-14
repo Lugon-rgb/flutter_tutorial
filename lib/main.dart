5e9a1555ec1f760c0401941346ae062075764ba0
Lugon <149012228+Lugon-rgb@users.noreply.github.com>
2026-05-13 21:24:07 -0300

    widget gridview

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: ListView.builder(
-          itemCount: names.length,
-          itemBuilder: (context, index) => ListTile(title: Text(names[index])),
+        body: Stack(
+          alignment: Alignment.bottomRight,
+          children: [
+            Container(height: 300, width: 300, color: Colors.deepPurple),
+            Container(height: 200, width: 200, color: Colors.deepPurple[400]),
+            Container(height: 100, width: 100, color: Colors.deepPurple[200]),
+          ],
         ),
       ),
     );
