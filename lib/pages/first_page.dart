b2528b975a0bd3aab2976618f5420756c1722580
Lugon <149012228+Lugon-rgb@users.noreply.github.com>
2026-05-13 22:07:23 -0300

    navigation bottomnavigationbar

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -1,35 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/pages/home_page.dart';
+import 'package:flutter_tutorial/pages/profile_page.dart';
+import 'package:flutter_tutorial/pages/settings_page.dart';
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+class FirstPage extends StatefulWidget {
+  FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  int _selectedIndex = 0;
+
+  void _navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List _pages = [HomePage(), SettingsPage(), ProfilePage()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Center(child: Text("1st Page"))),
-      drawer: Drawer(
-        backgroundColor: Colors.deepPurple[100],
-        child: Column(
-          children: [
-            DrawerHeader(child: Icon(Icons.favorite)),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("H O M E"),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, '/homepage');
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text("S E T T I N G S"),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, '/settingspage');
-              },
-            ),
-          ],
-        ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomBar,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "H O M E"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "P R O F I L E",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "S E T T I N G S",
+          ),
+        ],
       ),
     );
   }
--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -5,8 +5,6 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Center(child: Text("Settings Page"))),
-    );
+    return Scaffold(body: Center(child: Text("Settings Page")));
   }
 }
